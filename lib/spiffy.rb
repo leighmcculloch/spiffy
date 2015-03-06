8959ab7b6447bc2bf07dd54a10a37a59d3b7b6d0
Leigh McCulloch <leigh@mcchouse.com>
2015-03-06 08:20:13 -0800

    Initial application.

---
+++ lib/spiffy.rb
@@ -0,0 +1,38 @@
+require "github/markup"
+require "redcarpet"
+require "RedCloth"
+require "rdoc"
+require "org-ruby"
+require "creole"
+require "wikicloth"
+require "asciidoctor"
+require "haml"
+
+module Spiffy
+  def self.markup_to_html(markup_file, css_file: nil, template_file: nil)
+    markup_file_name = File.basename(markup_file, ".*")
+    markup = File.open(markup_file, "r:UTF-8", &:read)
+
+    html = GitHub::Markup.render(markup_file, markup)
+    
+    if css_file
+      css = File.open(css_file, "r:UTF-8", &:read)
+    end
+
+    if template_file
+      template_ext = File.extname(template_file)
+      template = File.open(template_file, "r:UTF-8", &:read)
+      html = case template_ext
+             when ".erb"
+               ERB.new(template).result { |section| case section; when :css; css; when :body; html; end }
+             when ".haml"
+               Haml::Engine.new(template).render { |section| case section; when :css; css; when :body; html; end }
+             else
+               raise "Template file #{template_file} unsupported. Only .erb or .haml are supported."
+             end
+    end
+
+    out_file = "#{markup_file_name}.html"
+    File.open(out_file, "w:UTF-8") { |f| f.write(html) }
+  end
+end
---
+++ spiffy.gemspec
@@ -0,0 +1,25 @@
+Gem::Specification.new do |s|
+  s.name        = "spiffy"
+  s.version     = "0.0.1"
+  s.date        = "2015-03-06"
+  s.summary     = "A markup to HTML & PDF converter."
+  s.description = "A markup to HTML & PDF converter. Supports all markups supported by Github, including markdown, rdoc, etc."
+  s.authors     = ["Leigh McCulloch"]
+  s.email       = "leigh@mcchouse.com"
+  s.files       = [
+    "lib/spiffy.rb",
+    "defaults/template.haml"
+  ]
+  s.homepage    = "http://rubygems.org/gems/hola"
+  s.license     = "BSD-3-Clause"
+  s.executables = [ "spiffy" ]
+  s.add_runtime_dependency "github-markup", "~> 1.3", ">= 1.3.3"
+  s.add_runtime_dependency "redcarpet", "~> 3.2", ">= 3.2.2"
+  s.add_runtime_dependency "RedCloth", "~> 4.2", ">= 4.2.9"
+  s.add_runtime_dependency "rdoc", "3.6.1"
+  s.add_runtime_dependency "org-ruby", "~> 0.9", ">= 0.9.12"
+  s.add_runtime_dependency "creole", "~> 0.5", ">= 0.5.0"
+  s.add_runtime_dependency "wikicloth", "~> 0.8", ">= 0.8.2"
+  s.add_runtime_dependency "asciidoctor", "~> 1.5", ">= 1.5.2"
+  s.add_runtime_dependency "haml", "~> 4.0", ">= 4.0.6"
+end
