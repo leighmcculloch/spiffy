8adb09a7fe069f774b82f47c499f713022b46c2a
Ian Atha <ian@mamabear.io>
2016-08-22 03:23:14 -0700

    Support for YAML-encoded metadata per document

--- lib/spiffy.rb
+++ lib/spiffy.rb
@@ -2,16 +2,24 @@ require "fileutils"
 require "github/markup"
 require "haml"
 require "pdfkit"
+require "yaml"
 
 module Spiffy
   def self.markup_to_html(markup_file, css_file: nil, template_file: nil, output_html: true, output_pdf: false, output_dir: nil)
     markup_file_ext = File.extname(markup_file)
     markup_file_name = File.basename(markup_file, ".*")
     markup_file_directory = File.dirname(markup_file)
-    markup = File.open(markup_file, "r:UTF-8", &:read)
+    contents = File.open(markup_file, "r:UTF-8", &:read)
 
-    html = GitHub::Markup.render(markup_file, markup)
-    
+    markup_pieces = contents.split("\n---\n")
+    metadata, html = if markup_pieces.length == 2
+      [ YAML.load(markup_pieces.first), GitHub::Markup.render(markup_file, markup_pieces.last) ]
+    elsif markup_pieces.length == 1
+      [ {}, GitHub::Markup.render(markup_file, contents) ]
+    else
+      raise "Found too many documents separated by --- in file #{markup_file}"
+    end
+
     if css_file
       css = File.open(css_file, "r:UTF-8", &:read)
     end
@@ -21,7 +29,8 @@ module Spiffy
       template = File.open(template_file, "r:UTF-8", &:read)
       local_variables = {
         base_url: "file://#{File.absolute_path(markup_file_directory)}/",
-        file: markup_file
+        file: markup_file,
+        meta: metadata,
       }
       html = case template_ext
              when ".erb"
